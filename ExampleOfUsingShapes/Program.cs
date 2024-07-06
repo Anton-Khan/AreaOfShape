7b627feff9324c4f147f57881daa5a9a50101c61
Anton-Khan <antonkharkov1404@gmail.com>
2024-07-06 03:13:05 +0300

    Example added

---
+++ ExampleOfUsingShapes/Program.cs
@@ -0,0 +1,9 @@
+﻿using AreaOfShape;
+
+var shapes = new List<IShapeWithArea>();
+
+shapes.Add(new Triangle(3, 5, 4));
+shapes.Add(new Circle(10));
+
+foreach(var shape in shapes)
+    Console.WriteLine(shape.CalculateArea());
